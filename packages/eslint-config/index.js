0b41a5c1472319f074d339d9aeb9fbd4008f0876
yanlei <yanlei@micronet.ltd>
2023-08-23 22:55:54 +0800

    feat: 添加eslint pkg 子包

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    './rules/base/best-practices',
+    './rules/base/possible-errors',
+    './rules/base/style',
+    './rules/base/variables',
+    './rules/base/es6',
+    './rules/imports',
+  ].map(require.resolve),
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: true,
+      jsx: true,
+    },
+  },
+  root: true,
+};
